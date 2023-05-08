5cb7c7e10ad9da71b91a86be3500435a5b732d57
Malinowsk <juanrago2015@gmail.com>
2023-05-08 10:01:33 -0300

    Avances en el delete y edit del carrito de compras

--- fruteria/urls.py
+++ fruteria/urls.py
@@ -2,15 +2,19 @@
 from django.contrib import admin
 from django.urls import path
 
-from fruteria.views import presentar_home , presentar_catalogo , presentar_contacto , presentar_about , presentar_detail , presentar_mensaje, add_fruit, show_cart
+from fruteria.views import presentar_home , presentar_catalogo , presentar_contacto , presentar_about , presentar_detail , presentar_mensaje, add_fruit, show_cart, delete_fruit, edit_fruit
 
 urlpatterns = [
     path("", presentar_home ,  name='home'),
     path("catalogo/", presentar_catalogo ,  name='catalogo'),
     path("contacto/", presentar_contacto ,  name='contacto'),
     path("about/", presentar_about ,  name='about'),
-    path("detail/<id>", presentar_detail ,  name='detail'),
+    path("detail/<id>/<mensaje>", presentar_detail ,  name='detail'),
     path("mensaje/", presentar_mensaje ,  name='mensaje'),
     path("cart/add/<fruit_id>", add_fruit ,  name='cart_add'),
+    path("cart/delete/<fruit_id>", delete_fruit ,  name='cart_delete'),
+    path("cart/edit/<id>/<mensaje>", presentar_detail ,  name='cart_edit'),
+    path("cart/edit-confirm/<fruit_id>", edit_fruit ,  name='cart_Edit_confirm'),
+    
     path("cart/", show_cart ,  name='cart'),
 ]
--- fruteria/views.py
+++ fruteria/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from fruteria.models import Fruit , PurchaseCart
 from fruteria.forms import PurchaseCartFormulario
 
@@ -42,18 +43,17 @@ def presentar_about(request):
     return http_responde
 
 
-def presentar_detail(request, id):
+def presentar_detail(request, id,mensaje=None):
     formulario =  PurchaseCartFormulario()
     fruta = Fruit.objects.get(id = id)
-    contexto = {"fruta": fruta, "form":formulario}
+    atributo_url= request.path.split('/')[1]
+    contexto = {"fruta": fruta, "form":formulario, "mensaje":mensaje,"atributo_url":atributo_url}
     http_responde = render(
         request=request,
         template_name='fruteria/detalle.html',
         context=contexto,
     )
     return http_responde
-
-
 
 def presentar_mensaje(request):
     contexto = {}
@@ -70,27 +70,25 @@ def add_fruit(request,fruit_id):
     if request.method == "POST":
         formulario = PurchaseCartFormulario(request.POST)
         if formulario.is_valid():
-            data = formulario.cleaned_data  # es un diccionario
             fruit = Fruit.objects.get(id=fruit_id)
-            quantity = data["quantity"]
-            purchase_cart = PurchaseCart(fruit=fruit, user= request.user, quantity=quantity , state = "en_carrito")  # lo crean solo en RAM
-            purchase_cart.save()  # Lo guardan en la Base de datos
-            http_responde = render(
-                request=request,
-                template_name='fruteria/mensaje.html',
-            )
-            return http_responde
+            if not PurchaseCart.objects.filter(user=request.user,fruit=fruit,state="en_carrito").exists(): 
+                data = formulario.cleaned_data  # es un diccionario
+                quantity = data["quantity"]
+                purchase_cart = PurchaseCart(fruit=fruit, user= request.user, quantity=quantity , state = "en_carrito")  # lo crean solo en RAM
+                purchase_cart.save()  # Lo guardan en la Base de datos
+                http_responde = render(
+                    request=request,
+                    template_name='fruteria/mensaje.html',
+                )
+                return http_responde
+            else:
+                mensaje='Ya posee en su carrito esta fruta, puede ingresar al carrito para modificar la cantidadde su compra.'
+                url_fallida = reverse('detail', args=[fruit_id,mensaje]) 
+                return redirect(url_fallida)
         
 def show_cart(request):
-    print("empieza")
-    print(request)
-    print(request.user)
-    print("ENTRE")
     carrito = PurchaseCart.objects.filter(user= request.user, state="en_carrito")
     total = sum([fruta.fruit.price*fruta.quantity for fruta in carrito])
-    print(carrito)
-    print(total)
-    print("TERMINA")
     contexto = {"carrito": carrito, "total": total}
     http_responde = render(
         request=request,
@@ -98,3 +96,25 @@ def show_cart(request):
         context=contexto,
     )
     return http_responde
+
+def delete_fruit(request, fruit_id):
+    fruit = Fruit.objects.get(id=fruit_id)
+    cart_fruit = PurchaseCart.objects.get(fruit=fruit, user= request.user, state = "en_carrito")
+    cart_fruit.delete()
+    url = reverse('cart') 
+    return redirect(url)
+
+
+
+def edit_fruit(request,fruit_id):
+  
+    if request.method == "POST":
+        formulario = PurchaseCartFormulario(request.POST)
+        if formulario.is_valid():
+            fruit = Fruit.objects.get(id=fruit_id)
+            purchase_cart = PurchaseCart.objects.get(fruit=fruit, user= request.user, state = "en_carrito")
+            data = formulario.cleaned_data  # es un diccionario
+            purchase_cart.quantity = data["quantity"]
+            purchase_cart.save()  # Lo guardan en la Base de datos
+            url = reverse('cart') 
+            return redirect(url)
